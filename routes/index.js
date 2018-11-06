bee8431b6d3ff1d8d40b120e22cff277c03aa570
prithivi77 <prithivi.monish@gmail.com>
2018-11-06 12:39:56 +0800

    dont know why this was removed

--- routes/index.js
+++ routes/index.js
@@ -16,56 +16,52 @@ const UserMeeting = require('../models/usermeeting');
 const userController = require('../controllers/user');
 
 
-  User.sync();
-  Meeting.sync();
-  Slot.sync();
-  Attendee.sync();
-  UserMeeting.sync();
-  const x = bcrypt.hashSync('1234');
-  User.findOrCreate({
-      where: {
-          email: 'john@mail.com'
-        },
-      defaults: {
-        uFname: 'john',
-        password: x,
-        uType: 'admin'
-        }
+User.sync().then(()=>{
+    Meeting.sync().then(()=>{
+        UserMeeting.sync().then(()=>{
+            const x = bcrypt.hashSync('1234');
+            User.findOrCreate({
+                where: {
+                    email: 'john@mail.com'
+                  }, 
+                defaults: {
+                  uFname: 'john',
+                  uLname: 'nhoj',
+                  password: x,
+                  uType: 'admin'
+                  }
+              });
+              User.findOrCreate({
+                  where: {
+                      email: 'adam@mail.com'
+                    }, 
+                  defaults: {
+                    uFname: 'adam',
+                    uLname: 'mada',
+                    password: x,
+                    uType: 'convener'
+                    }
+                });
+          
+               User.findOrCreate({
+                    where: {
+                      email: 'saed@mail.com'
+                  },
+                 defaults: {
+                  uFname: 'saed',
+                  uLname: 'daes',
+                  password: x,
+                  uType: 'organiser'
+                  }
+              });
+        });
+    }).then(()=>{
+        Slot.sync().then(()=>{
+            Attendee.sync();
+        })
     });
-    User.findOrCreate({
-        where: {
-            email: 'adam@mail.com'
-          },
-        defaults: {
-          uFname: 'adam',
-          password: x,
-          uType: 'convener'
-          }
-      });
-
-     User.findOrCreate({
-          where: {
-            email: 'sdep@mail.com'
-        },
-       defaults: {
-        uFname: 'sdep',
-        password: x,
-        uType: 'organiser'
-        }
-    }).spread((user, created) => {
-        console.log(user.get({plain: true}))
-        // console.log('created: '+created)
-    });
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
+  });
+  
 
 var sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
