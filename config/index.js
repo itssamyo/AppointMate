81082871704acdd2542e63eed862652c967d4244
prithivi77 <prithivi.monish@gmail.com>
2018-11-15 14:00:07 +0800

    pgdb

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ module.exports = {
 	secret: 'ilovescotchyscotch',
 	email: 'appointmateLogicg8@gmail.com',
 	password: 'gGn64TKV9r45',
-	dburl: 'postgres://localhost:5432/lsapp'
+	dburl: 'postgres://ozssjmnwquapwy:1529c58568fde98ae8d347b33baea9076de01b3f5ce3e80c9e08796d53a3267f@ec2-50-17-225-140.compute-1.amazonaws.com:5432/dckou30lg9fa94'
 };
