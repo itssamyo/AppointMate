3b83c374ac1f2ef22325bd30d82dac6e26546002
prithivi77 <prithivi.monish@gmail.com>
2018-11-23 22:12:40 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ module.exports = {
 	password: 'gGn64TKV9r45',
 	dburl: 'postgres://ozssjmnwquapwy:1529c58568fde98ae8d347b33baea9076de01b3f5ce3e80c9e08796d53a3267f@ec2-50-17-225-140.compute-1.amazonaws.com:5432/dckou30lg9fa94',	
 	//dburl: 'postgres://localhost:5432/lsapp',
-	surl: 'http://localhost:3000',
-	//surl: 'https://appointmate.herokuapp.com'
+	//surl: 'http://localhost:3000',
+	surl: 'https://appointmate.herokuapp.com'
 	
 
 };
